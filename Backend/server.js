977c567d1edaa80a0cee4914ff3e7ec76a81473e
Daanish Baig <baigdaanish0@gmail.com>
2026-04-27 10:09:10 +0530

    fixed mongodb atlas connection

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/constructionDB");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("Mongo error:", err));
 
 const Report = mongoose.model("Report", {
   client: String,
